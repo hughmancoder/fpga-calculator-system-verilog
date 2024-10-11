e16998b32680d77fe4d27d74843425a50a939144
Hugh Signoriello <53002705+hughmancoder@users.noreply.github.com>
2024-10-11 14:44:19 +1030

    Update input_handler.sv

--- input_handler.sv
+++ input_handler.sv
@@ -1,4 +1,6 @@
-module operand_display_handler (
+`timescale 1ns / 1ps
+
+module input_handler (
     input clk,
     input reset,
     input confirm,
@@ -7,13 +9,10 @@ module operand_display_handler (
     input [2:0] state,
     output reg signed [5:0] operand1,
     output reg signed [5:0] operand2,
-    output reg signed [5:0] display_result,
+    output reg signed [5:0] operand_input_display,
     output reg display_mode
 );
 
-    reg display_mode_change_prev;
-
-    // NOTE: could use package for shared type
     typedef enum logic [2:0] {
         S0_IDLE      = 3'b000,
         S1_OP1       = 3'b001,
@@ -23,27 +22,38 @@ module operand_display_handler (
         S5_CALCULATE = 3'b101
     } state_t;
 
+    reg display_mode_change_prev;
+
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             operand1 <= 6'sb000000;
             operand2 <= 6'sb000000;
-            display_result <= 6'sb000000;
+            operand_input_display <= 6'sb000000;
             display_mode_change_prev <= 0;
             display_mode <= 0;
-            state <=  S0_IDLE;
         end else begin
-
-            // posedge detection
+            // Positive edge detection for display mode change
             if (display_mode_change_prev == 0 && display_mode_change == 1) begin
                 display_mode <= ~display_mode;  
             end
             display_mode_change_prev <= display_mode_change;
 
-            end
             case (state)
-                S1_OP1: display_result <= $signed({1'b0, operand_input});  
-                S2_OP1_WR: if (confirm) operand1 <= $signed({1'b0, operand_input}); 
-                S3_OP2: display_result <= $signed({1'b0, operand_input});  
+                S1_OP1: begin
+                operand_input_display <= $signed({1'b0, operand_input});  
+                if (confirm) begin
+                    operand1 <= $signed({1'b0, operand_input}); 
+                end
+            end
+            S2_OP1_WR: if (confirm) operand1 <= $signed({1'b0, operand_input}); 
+            S3_OP2: begin
+                operand_input_display <= $signed({1'b0, operand_input});  
+                if (confirm) begin
+                    operand2 <= $signed({1'b0, operand_input});  
+                end
+            end
+
+                S3_OP2: operand_input_display <= $signed({1'b0, operand_input});  
                 S4_OP2_WR: if (confirm) operand2 <= $signed({1'b0, operand_input});  
                 default:;
             endcase
